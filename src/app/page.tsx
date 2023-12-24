14bc68442f4aef069b3d39745748bb8620f91b9c
khankluan24 <longtran24062000@gmail.com>
2023-12-24 21:58:03 +0700

    Feat: Implement lesson 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,47 @@
-import Image from 'next/image'
+import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-export default function Home() {
+interface Recipe {
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+  id: string;
+}
+
+const fetchRecipes = async (): Promise<Recipe[]> => {
+  const result = await fetch("http://localhost:3001/recipes");
+  return result.json();
+};
+export default async function Home() {
+  const recipes = await fetchRecipes();
   return (
-   <main>
-    home
-   </main>
-  )
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((el) => (
+          <Card key={el.id} className="flex flex-col justify-between">
+            <CardHeader className="flex gap-4 items-center">
+              <CardTitle>{el.title}</CardTitle>
+              <CardDescription>{el.time} mins to cook.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>{el.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <button>View recipe</button>
+              {el.vegan && <p>Vegan !!!</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
+  );
 }
